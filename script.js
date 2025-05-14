a1f905683f22158c158a91899c4dca38928e133b
Matteda Ramesh <85834506+rummy143679@users.noreply.github.com>
2025-05-14 22:30:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let obj = {};
+	for (let index = 0; index < str.length; index++) {
+		if(!(str.charAt(index) in obj)){
+			obj[str.charAt(index)] = 1;
+		}else{
+			obj[str.charAt(index)] += 1
+		}
+	}
+	for(let key in obj){
+		if(obj[key] == 1){
+			return key;
+		}
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
